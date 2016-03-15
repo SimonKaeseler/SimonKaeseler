362510344bd13685afb510bc8302ab1886b9dd65
SimonKaeseler <simonkaeseler@gmail.com>
2016-03-15 08:11:39 +0100

    build ex8

--- I4IKN/exercise8/Exercise_8_c#/file_server/file_server.cs
+++ I4IKN/exercise8/Exercise_8_c#/file_server/file_server.cs
@@ -2,109 +2,106 @@ using System;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 
 namespace tcp
 {
-	class Server
-	{
-		/// <summary>
-		/// The PORT
-		/// </summary>
-		public const int PORT = 9000;
-		/// <summary>
-		/// The BUFSIZE
-		/// </summary>
-		public const int BUFSIZE = 1000;
+    class Server
+    {
+        /// <summary>
+        /// The PORT
+        /// </summary>
+        public const int PORT = 9000;
+        /// <summary>
+        /// The BUFSIZE
+        /// </summary>
+        public const int BUFSIZE = 1000;
 
-		public int fileLength;
-		public byte[] bytes;
-		public string data;
+        public int fileLength;
+     
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Server"/> class.
+        /// Opretter en socket.
+        /// Venter på en connect fra en klient.
+        /// Modtager filnavn
+        /// Finder filstørrelsen
+        /// Kalder metoden sendFile
+        /// Lukker socketen og programmet
+        /// </summary>
+        private Server()
+        {
+            var serverSocket = new TcpListener(IPAddress.Any,PORT);
+            var client = new TcpClient();
+            var reader = client.GetStream();
+            serverSocket.Start();
+            Console.WriteLine("Server is started...");
 
-		private TcpListener serverSocket;
-		private TcpClient client;
-		private NetworkStream reader;
-		public int RequestCount { get; private set; }
+            try
+            {
+                //Returns a TCPClient and saves it in "client"
+                client = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Connection established...");
+                reader = client.GetStream();
+                
+                string data = LIB.readTextTCP(reader);
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Server"/> class.
-		/// Opretter en socket.
-		/// Venter på en connect fra en klient.
-		/// Modtager filnavn
-		/// Finder filstørrelsen
-		/// Kalder metoden sendFile
-		/// Lukker socketen og programmet
-		/// </summary>
-		private Server()
-		{
-			bytes = new byte[BUFSIZE];
-			serverSocket = new TcpListener(IPAddress.Any,PORT);
-			serverSocket.Start();
-			Console.WriteLine("Server is started...");
+                Console.WriteLine("Received: {0}", data);
+                SendFile(data, fileLength, reader);
+            }
+            catch (Exception)
+            {
+                throw new SocketException();
+            }
+            
+            client.Close();
+            serverSocket.Stop();
+        }
 
-			try
-			{
-				client = serverSocket.AcceptTcpClient();
-				Console.WriteLine("Connection established...");
-			}
-			catch (Exception)
-			{
+        /// Sends the file.
+        /// </summary>
+        /// <param name='fileName'>
+        /// The filename.
+        /// </param>
+        /// <param name='fileSize'>
+        /// The filesize.
+        /// </param>
+        /// <param name='io'>
+        /// Network stream for writing to the client.
+        /// </param>
+        private void SendFile(String fileName, int fileSize, NetworkStream io)
+        {
+            //The filecheck returns the size of the file, if it excists
+            long filesize = LIB.check_File_Exists(fileName);
+            if (filesize != 0)
+            {
+                var file = File.Open(fileName, FileMode.Open);
+                byte[] bytes = new byte[BUFSIZE];
 
-				throw new SocketException();
-			}
+                //iterates through the fie while it reads 1000 bits at a time.
+                for (int i = 0; i < fileSize; i += BUFSIZE)
+                {
+                    //Reads the file "bytes" with zero offset 
+                    var size = file.Read(bytes, 0, BUFSIZE);
+                    io.Write(bytes,0,size);
+                }
+                file.Close();
+            }
 
-			reader = client.GetStream();
-			fileLength = reader.Read(bytes, 0, bytes.Length);
-			sendFile(data,fileLength,reader);
+        }
 
-		}
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name='args'>
+        /// The command-line arguments.
+        /// </param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Server starts...");
 
-		/// Sends the file.
-		/// </summary>
-		/// <param name='fileName'>
-		/// The filename.
-		/// </param>
-		/// <param name='fileSize'>
-		/// The filesize.
-		/// </param>
-		/// <param name='io'>
-		/// Network stream for writing to the client.
-		/// </param>
-		private void sendFile(String fileName, int fileSize, NetworkStream io)
-		{
-			while (fileSize != 0)
-			{
-				// Translate data bytes to a ASCII string.
-				data = System.Text.Encoding.ASCII.GetString(bytes, 0, fileSize);
-
-				Console.WriteLine(String.Format("Received: {0}", data));
-
-				// Process the data sent by the client.
-				data = data.ToUpper();
-
-				byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-				// Send back a response.
-				reader.Write(msg, 0, msg.Length);
-				Console.WriteLine(String.Format("Sent: {0}", data));
-
-				fileSize = reader.Read(bytes, 0, bytes.Length);
-			}
-
-		}
-
-		/// <summary>
-		/// The entry point of the program, where the program control starts and ends.
-		/// </summary>
-		/// <param name='args'>
-		/// The command-line arguments.
-		/// </param>
-		public static void Main(string[] args)
-		{
-			Console.WriteLine("Server starts...");
-
-
-			new Server();
-		}
-	}
+           
+            new Server();
+        }
+    }
 }
 
