331e1d23ad21bb20f73a2c6da530e3a5b61a9391
Simon Kæseler <SimonKaeseler@gmail.com>
2016-03-16 16:44:27 +0100

    Startet på exercise 9

---
+++ I4IKN/exercise9/UserDatagramProtocol/UserDatagramProtocol/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace UserDatagramProtocol
+{
+	class MainClass
+	{
+		private const int PORT = 9000;
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
