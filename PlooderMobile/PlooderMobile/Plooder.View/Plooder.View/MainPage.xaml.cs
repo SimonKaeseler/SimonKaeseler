c568e822530a78f59c6062cdb4894b95aa2833c1
Simon Kæseler <simonkaeseler@gmail.com>
2016-04-15 11:45:06 +0200

    MAYBE ITS WORKING!

--- PlooderMobile/PlooderMobile/Plooder.View/Plooder.View/MainPage.xaml.cs
+++ PlooderMobile/PlooderMobile/Plooder.View/Plooder.View/MainPage.xaml.cs
@@ -1,13 +1,31 @@
-﻿using Xamarin.Forms;
+﻿using System.Collections.ObjectModel;
+using Xamarin.Forms;
 
 namespace Plooder.View
 {
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<Item> Items { get; set; }
         public MainPage()
         {
             InitializeComponent();
+
+            this.BindingContext = Items;
+            ItemView.ItemsSource = Items;
+
+            Items.Add(new Item("item1"));
         }
         
+
+    }
+
+    public class Item
+    {
+        string name;
+
+        public Item(string name)
+        {
+            this.name = name;
+        }
     }
 }
