840ad550affcaebaec72007508970b8df5c1edb0
SimonKaeseler <simonkaeseler@gmail.com>
2016-05-04 10:08:55 +0200

    fix

--- I4IKN/exercise13/Link/Link.cs
+++ I4IKN/exercise13/Link/Link.cs
@@ -52,40 +52,71 @@ namespace Linklaget
 		/// <param name='size'>
 		/// Size.
 		/// </param>
+//		public void send (byte[] buf, int size)
+//		{
+//	    	// TO DO Your own code
+//			//Convert byte array to strings, send each string, terminate with /n
+//			//OBS: A 
+//
+//			char[] charsToSend = new char[size+3];
+//			int currentIndex = 0;
+//
+//			for (currentIndex = 0; currentIndex < size; currentIndex++) 
+//			{
+//				if (buf [currentIndex] == 'A') {
+//					charsToSend [currentIndex] = 'B';
+//					currentIndex++;
+//					charsToSend [currentIndex] = 'C';
+//				} else if (buf [currentIndex] == 'B') {
+//					charsToSend [currentIndex] = 'B';
+//					currentIndex++;
+//					charsToSend [currentIndex] = 'D';
+//				} else 
+//				{
+//					charsToSend [currentIndex] = (char)buf [currentIndex];
+//				}
+//			}
+//
+//			charsToSend[currentIndex] = 'A';
+//			currentIndex ++;
+//			charsToSend[currentIndex] = '\n';
+//			string package = null;
+//
+//			foreach (char c in charsToSend) {package += c;};
+//
+//			Console.WriteLine (package);
+//			serialPort.Write (package);
+//		}
 		public void send (byte[] buf, int size)
 		{
-	    	// TO DO Your own code
-			//Convert byte array to strings, send each string, terminate with /n
-			//OBS: A 
+			List<byte> toSend= new List<byte>();
+			toSend.Add ((byte)'A');
 
-			char[] charsToSend = new char[size+3];
-			int currentIndex = 0;
-
-			for (currentIndex = 0; currentIndex < size; currentIndex++) 
+			for (int i = 0; i < size; i++) 
 			{
-				if (buf [currentIndex] == 'A') {
-					charsToSend [currentIndex] = 'B';
-					currentIndex++;
-					charsToSend [currentIndex] = 'C';
-				} else if (buf [currentIndex] == 'B') {
-					charsToSend [currentIndex] = 'B';
-					currentIndex++;
-					charsToSend [currentIndex] = 'D';
-				} else 
+				if (buf [i] == (byte)'A') 
 				{
-					charsToSend [currentIndex] = (char)buf [currentIndex];
+					toSend.Add ((byte)'B');
+					toSend.Add ((byte)'C');
 				}
+				else if (buf [i] == (byte)'B')
+				{
+					toSend.Add ((byte)'B');
+					toSend.Add ((byte)'D');
+				}
+				else
+					toSend.Add (buf [i]);
 			}
 
-			charsToSend[currentIndex] = 'A';
-			currentIndex ++;
-			charsToSend[currentIndex] = '\n';
-			string package = null;
+			toSend.Add ((byte)'A');
 
-			foreach (char c in charsToSend) {package += c;};
+			//for (int i = 0; i < size; i++) 
+			//{
+			//	Console.WriteLine (buf [i]);
+			//}
 
-			Console.WriteLine (package);
-			serialPort.Write (package);
+			var t = toSend.ToArray ();
+			serialPort.Write (t, 0, toSend.Count);
 		}
 
 		/// <summary>
